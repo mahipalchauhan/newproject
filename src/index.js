a9837296d09ca58b52ba6ebcdcb1adf09a70f95d
mahipalchauhan <msc08022004@gmail.com>
2025-08-26 11:09:36 +0530

    "re utils"

--- src/index.js
+++ src/index.js
@@ -17,11 +17,11 @@ app.use(cors({
 }
 ))
 
-app.use(express.json({ 
-    limit:"20kb"}
-))
-
+app.use(express.json({ limit:"20kb"}))
 app.use(express.urlencoded({extended:true,limit:"20kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
 
 connectDB()
 .then(()=>{
